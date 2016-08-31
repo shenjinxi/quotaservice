234ef40a904aac5068a957f87fbdce279fecf6df
Steven Davidovitz <steved@squareup.com>
2016-08-31 16:36:47 -0700

    add a new AllowRequest flag to control max wait time override
    Previously, any AllowRequest sent would always override the configured
    max_wait_time. Since this isn't expected and proto3 has no default
    values, we add a new flag.
    
    Also added some code to automatically configure yaml names for config
    proto.

--- bin/compile_protos.sh
+++ bin/compile_protos.sh
@@ -3,7 +3,10 @@
 # Licensed under the Apache License, Version 2.0
 # Details: https://raw.githubusercontent.com/maniksurtani/quotaservice/master/LICENSE
 
+set -ex
+
 protoc --go_out=plugins=grpc:. ./protos/*.proto --proto_path ./
 protoc --go_out=plugins=grpc:. ./protos/config/*.proto --proto_path ./
+sed -i '' -e 's/\(json:"\([^,]*\),omitempty"\)/\1 yaml:"\2"/g' ./protos/config/configs.pb.go
 
 echo "Protos compiled. If you made any changes to protos/config/configs.proto, then please read protos/config/README.md now."
