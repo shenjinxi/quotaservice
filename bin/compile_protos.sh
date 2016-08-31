ace66637a78d0f5fe78d2be13378dd1e24e9ff26
Steven Davidovitz <steved@squareup.com>
2016-08-31 16:44:57 -0700

    test compiling protos in travis

--- bin/compile_protos.sh
+++ bin/compile_protos.sh
@@ -7,6 +7,8 @@ set -ex
 
 protoc --go_out=plugins=grpc:. ./protos/*.proto --proto_path ./
 protoc --go_out=plugins=grpc:. ./protos/config/*.proto --proto_path ./
-sed -i '' -e 's/\(json:"\([^,]*\),omitempty"\)/\1 yaml:"\2"/g' ./protos/config/configs.pb.go
+
+# need the .2 extension so that this works on os x and linux equally
+sed -i.2 -e 's/\(json:"\([^,]*\),omitempty"\)/\1 yaml:"\2"/g' ./protos/config/configs.pb.go && rm ./protos/config/configs.pb.go.2
 
 echo "Protos compiled. If you made any changes to protos/config/configs.proto, then please read protos/config/README.md now."
--- test/script.sh
+++ test/script.sh
@@ -5,9 +5,21 @@ set -ex
 case "$TEST" in
   "UI")
     cd admin/public/
+    nvm install 6
     npm install
     npm run lint
     npm run dist
+    ;;
+  "COMPILE_PROTOS")
+    wget https://github.com/google/protobuf/releases/download/v3.0.0/protoc-3.0.0-linux-x86_64.zip
+    unzip -d protoc protoc-3.0.0-linux-x86_64.zip
+
+    go get -u github.com/golang/protobuf/protoc-gen-go
+
+    export PATH="$PATH:./protoc/bin/"
+
+    # Make sure protos haven't changed + test compile_protos.sh
+    bin/compile_protos.sh && git diff --exit-code -- protos/
     ;;
   *)
     go test -race -v ./...
